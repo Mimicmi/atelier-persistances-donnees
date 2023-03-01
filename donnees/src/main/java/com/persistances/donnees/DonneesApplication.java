064ea258723a7cf08838b2cefb429069e1de0b2c
Simon <simon@farmleap.com>
2023-03-01 10:19:45 +0100

    install back

---
+++ donnees/src/main/java/com/persistances/donnees/DonneesApplication.java
@@ -0,0 +1,13 @@
+package com.persistances.donnees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonneesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonneesApplication.class, args);
+	}
+
+}
---
+++ donnees/src/test/java/com/persistances/donnees/DonneesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persistances.donnees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonneesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
