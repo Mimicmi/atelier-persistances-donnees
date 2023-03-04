c175e61a46ffa5fdeb581bd8bcb6510572dbda03
Simon <simon@farmleap.com>
2023-03-04 11:46:05 +0100

    feature/getProduits marche

--- donnees/src/main/java/com/persistances/donnees/controller/ProduitController.java
+++ donnees/src/main/java/com/persistances/donnees/controller/ProduitController.java
@@ -1,9 +1,12 @@
 package com.persistances.donnees.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,5 +41,11 @@ public class ProduitController {
         Magasin magasin = magasinRepository.findById(magasinId).get();
         produit.getMagasin().add(magasin);
         return produitRepository.save(produit);
+    }
+
+    @GetMapping("/produits")
+    public ResponseEntity<List<Produit>> getProduits() {
+        List<Produit> produits = produitRepository.findAll();
+        return new ResponseEntity<>(produits, HttpStatus.OK);
     }
 }
